ca1df4021e77b2324d66bdfdda03426acac5ce31
eagormle <79175765+eagormle@users.noreply.github.com>
2021-02-16 17:40:31 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var name = prompt("What is your name?");
+
+var color = prompt("What is your favorite color?");
+
+document.body.querySelector(".message").innerHTML="My name is "+name+". My favorite color is "+color;
